10e3f7020b8d543d4908bb92e7bfbec905919a69
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2023-12-07 07:58:33 -0600

    first commit

---
+++ src/main/java/com/example/javadockercd/JavaDockerCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.javadockercd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDockerCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDockerCdApplication.class, args);
+	}
+
+}
