d24a23c0c0162dc475584d688c1d7ced18797815
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2023-12-07 19:32:21 -0600

    new test file

---
+++ src/main/java/com/example/javadockercd/Test.java
@@ -0,0 +1,12 @@
+package com.example.javadockercd;
+
+public class Test {
+
+    public Test() {
+        System.out.println("Hello from the test file!!!");
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
