4e51a460b6dd544f6eb00bd1979d3b744262beca
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2023-12-07 20:00:40 -0600

    added docker file

--- src/main/java/com/example/javadockercd/Test.java
+++ src/main/java/com/example/javadockercd/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
     public Test() {
         System.out.println("Hello from the test file!!!");
+        System.out.println("sample update");
     }
     @Override
     public String toString() {
